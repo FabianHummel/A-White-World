5d4362a02e9bdde55615b346b8223cc1773cb017
Fabian Hummel <hum22501@spengergasse.at>
2023-12-27 09:23:28 +0100

    Old changes

--- game/scripts/seashore/TestInteraction.cs
+++ game/scripts/seashore/TestInteraction.cs
@@ -5,8 +5,10 @@ using WhiteWorld.engine.scripts;
 namespace WhiteWorld.game.scripts.seashore;
 
 [DisallowMultipleInstances]
-public class TestInteraction : Interactable {
-    public override void OnInteract(GameObject interactor) {
+public class TestInteraction : Interactable
+{
+    public override void OnInteract(GameObject interactor)
+    {
         Engine.QueueDialogue("Box", "Hello! Let me introduce myself...");
         Engine.QueueDialogue("Box", "Guess what, I am a box!");
         Engine.QueueDialogue("Box", "But not like any other box...");
